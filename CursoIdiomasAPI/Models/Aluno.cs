d19a7c34159c41166340d755f889a9d20f0747a7
Thiago Augusto <taugusto964@gmail.com>
2024-10-21 09:22:43 -0300

    Create classes modelo

---
+++ CursoIdiomasAPI/Models/Aluno.cs
@@ -0,0 +1,12 @@
+﻿namespace CursoIdiomasAPI.Models
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; }
+        public string Nome { get; set; }
+        public string CPF {  get; set; }
+        public string email {  get; set; }
+        public List<Turma> Turmas { get; set; } = [];
+
+    }
+}
---
+++ CursoIdiomasAPI/Models/Turma.cs
@@ -0,0 +1,17 @@
+﻿namespace CursoIdiomasAPI.Models
+{
+    public class Turma
+    {
+        public int TurmaId { get; set; }
+        public string Codigo {  get; set; }
+        public Nivel Nivel { get; set; }
+        public List<Aluno> Alunos { get; set; } = [];
+
+    }
+    public enum Nivel
+    {
+        Iniciante = 0,
+        Intermediario = 1,
+        Avançado = 2
+    }
+}
