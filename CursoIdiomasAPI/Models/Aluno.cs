f5b0cc802708812be2ab6eba7e1e04d7203d51df
Thiago Augusto <taugusto964@gmail.com>
2024-10-21 09:27:40 -0300

    Instalaçao ef core

--- CursoIdiomasAPI/Models/Aluno.cs
+++ CursoIdiomasAPI/Models/Aluno.cs
@@ -5,7 +5,7 @@
         public int AlunoId { get; set; }
         public string Nome { get; set; }
         public string CPF {  get; set; }
-        public string email {  get; set; }
+        public string Email {  get; set; }
         public List<Turma> Turmas { get; set; } = [];
 
     }
